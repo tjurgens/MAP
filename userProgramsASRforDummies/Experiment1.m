40ed72502ee0f4626d54512f44300119e378ba53
Tim Jürgens <timjurgens@tim-jurgenss-macbook.local>
2011-11-17 21:17:20 +0000

    DFLT features with timing fixed now really

--- userProgramsASRforDummies/Experiment1.m
+++ userProgramsASRforDummies/Experiment1.m
@@ -39,8 +39,8 @@ xL.numCoeff = 20;
 xL.removeEnergyStatic = 0;
 
 %%%%% Group of params that will influence simulation run time %%%%%%%
-xL.numWavs = 1;%1000; %10;%MAX=8440
-testWavs = 1;%200;%5;MAX = 358
+xL.numWavs = 1000; %10;%MAX=8440
+testWavs = 200;%5;MAX = 358
 nzLevel = [-200 40:10:70];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
