28bc6f18eceb68dc4b9d3dea9c1b3a5c2ea77340
Tim Juergens <tim.juergens@uni-oldenburg.de>
2012-01-26 12:47:09 +0000

    ready for experiment 27

--- userProgramsTim/extract_envelope.m
+++ userProgramsTim/extract_envelope.m
@@ -9,6 +9,7 @@ hilberttransformed_insignal = abs(hilbert(insignal));
 % get filter coefficients for a 4th order butterworth low pass filter with 
 % specified cutoff_frequency
 [b,a]=butter(4, cutoff_frequency/(sfreq/2), 'low'); %
-envelope=filter(b,a, hilberttransformed_insignal);
+%use MATLABs filtfilt in order to get rid of the group delay
+envelope=filtfilt(b,a, hilberttransformed_insignal);
 
 outsignal = envelope;
