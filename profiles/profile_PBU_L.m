d3026332f2e49414d1cd465e0f2ae1cd0be41580
Tim Juergens <tim.juergens@uni-oldenburg.de>
2012-01-26 14:01:06 +0000

    ready for experiment 28

---
+++ profiles/profile_PBU_L.m
@@ -0,0 +1,46 @@
+function x = profile_PBU_L									
+x.Comments= {									
+	};								
+x.BFs= [   									
+	250	500	1000	2000	4000	6000	8000		
+	];								
+x.LongTone= [ 									
+	19.55	26.26	19.05	50.10	55.24	57.47	52.87		
+	];								
+x.ShortTone= [ 									
+	29.62	30.20	21.82	52.34	58.41	58.04	57.00		
+	];								
+x.IFMCFreq= [									
+	250	500	1000	2000	4000	6000	8000		
+	];								
+x.MaskerRatio=[    									
+	0.5	0.7	0.9	1	1.1	1.3	1.6		
+	];								
+x.IFMCs=[									
+	NaN	NaN	58.72	79.59	89.40	80.90	NaN		
+	NaN	NaN	45.72	72.59	80.60	68.34	NaN		
+	NaN	NaN	27.52	60.42	69.07	63.05	NaN		
+	NaN	NaN	21.75	61.19	62.67	62.45	NaN		
+	NaN	NaN	25.30	63.42	61.45	64.35	NaN		
+	NaN	NaN	33.30	72.96	66.18	67.78	NaN		
+	NaN	NaN	73.51	88.17	71.40	91.70	NaN		
+	];								
+x.IFMCs= x.IFMCs';									
+x.Gaps= [									
+	0.01	0.02	0.03	0.04	0.05	0.06	0.07	0.08	0.09
+	];								
+x.TMCFreq= [									
+	250	500	1000	2000	4000	6000	8000		
+	];								
+x.TMC= [									
+	NaN	NaN	NaN	NaN	NaN	NaN	NaN		
+	NaN	NaN	22.95	58.04	76.08	68.31	NaN		
+	NaN	NaN	NaN	NaN	NaN	NaN	NaN		
+	NaN	NaN	25.11	59.45	79.53	70.82	NaN		
+	NaN	NaN	24.15	59.21	75.03	67.37	NaN		
+	NaN	NaN	48.08	57.30	83.13	73.20	NaN		
+	NaN	NaN	NaN	NaN	NaN	NaN	NaN		
+	NaN	NaN	32.86	58.78	82.61	69.13	NaN		
+	NaN	NaN	NaN	NaN	NaN	NaN	NaN		
+	];								
+x.TMC = x.TMC';									
