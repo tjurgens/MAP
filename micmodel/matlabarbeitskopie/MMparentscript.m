7584cd09fde77281f6630d9486bb3085af18efb7
Tim Juergens <tim.juergens@uni-oldenburg.de>
2012-04-10 16:33:28 +0100

    ready for Experiment3

--- micmodel/matlabarbeitskopie/MMparentscript.m
+++ micmodel/matlabarbeitskopie/MMparentscript.m
@@ -29,7 +29,7 @@ else
     no_repetitions = 1;
 end
 
-subpath_to_save = 'MMEuklidRateHSRExp2'; %specify savepath here! DON'T USE UNDERLINES FOR SAVEPATHS
+subpath_to_save = 'MMEuklidRateLSRExp3'; %specify savepath here! DON'T USE UNDERLINES FOR SAVEPATHS
 %parentdir = 'C:\MAP\micmodel\';%'/scratch/tjurgens/MAP/micmodel/'; 
 addpath([parentdir 'matlabarbeitskopie' filesep]);
 
