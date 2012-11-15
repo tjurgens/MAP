048ae653364f5e8f6b85dfc315da0e830e202aa1
Tim@schroeder <tim.juergens@uni-oldenburg.de>
2012-11-15 15:40:44 +0100

    first chime own features (EXP_C1)

--- userProgramsTim/chime_FTHfeatures.m
+++ userProgramsTim/chime_FTHfeatures.m
@@ -1,3 +1,4 @@
+function chime_FTHfeatures(startindex,endindex)
 %Script for the generation of FTH features for CHiME
 clear all;
 load('/schroedersan/Corpora/CHiME/eval_chime_fhg/dir_struct/chime_filepaths.mat');
@@ -15,19 +16,20 @@ SHIFT = 10;     % sampling rate of features in [ms] (usually 10 ms)
     header.sampKind = 9;
     numCoeff = 14; %14 DCT coefficients
     
-for iCounter = 1:length(filepaths)
+for iCounter = startindex:endindex %1:length(filepaths)
+    
     actualfilepathname = [chime_root{2} filesep filepaths{iCounter}];
     [signal,sampleRate] = wavread(actualfilepathname);
     %take the mean from both channels as monaural signal
     signal = mean(signal,2);
     
-    %set signal to 60 dB SPL for MAP
-    
+    %multiply by two to get about a speech level of 60 dB regardless of SNR
+    signal = signal.*2; 
     
     %resample -> this step requires much time and might be unnecessary if I
     %get MAP to produce reasonable output for sfreq = 16000;
     signal = resample(signal,44100,sampleRate);
-    
+     
     %auditory model
     [ANprobabilityResponse, dt, myBFlist] = MAPwrap(signal, 44100, -1, 'Normal', 'probability', {';'});
     
@@ -45,8 +47,9 @@ for iCounter = 1:length(filepaths)
      %Features  <= deine Feature Matrix (columns: features, rows: frames)
     header.nSamples = size(ANfeatures,2);     % get number of frames
     header.sampSize = size(ANfeatures,1)*4;   % get number of features
-    strOutputFile = ['/schroedersan/tim/chime/features/' filepaths_own{iCounter}];
+    strOutputFile = ['/schroedersan/tim/Chime/feat1/' filepaths_own{iCounter}];
     % ^^zB (Endung own nicht vergessen)
 
     write_htk_with_header(ANfeatures,header,strOutputFile,byteswap); 
+   
 end
---
+++ userProgramsTim/chime_parallel.m
@@ -0,0 +1,13 @@
+
+
+load('/schroedersan/Corpora/CHiME/eval_chime_fhg/dir_struct/chime_filepaths.mat');
+nwavs = length(filepaths);
+startindex=[1:40:nwavs];
+endindex = [40:40:nwavs];
+
+for iCounter = 1:length(startindex)
+    matlabcommand_to_exe = ['chime_FTHfeatures(' num2str(startindex(iCounter)) ',' num2str(endindex(iCounter)) ');'];
+    job_name = ['CHiME' num2str(iCounter)];
+    jobID_string = qsub(matlabcommand_to_exe, job_name ,'','-q all.q');%'-r y');%
+end
+            
