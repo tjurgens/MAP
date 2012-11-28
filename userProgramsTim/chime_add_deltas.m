2421770419e0340759fd7a301e690b46a138ebd6
Tim@schroeder <tim.juergens@uni-oldenburg.de>
2012-11-28 11:28:39 +0100

    ready to calculate features for experiment C3

--- userProgramsTim/chime_add_deltas.m
+++ userProgramsTim/chime_add_deltas.m
@@ -1,6 +1,6 @@
 
 featureDirread = '/schroedersan/tim/Chime/feat1'; 
-featureDirwrite = '/schroedersan/tim/Chime/feat2'; 
+featureDirwrite = '/schroedersan/tim/Chime/feat3'; 
 byteswap = 1;   %serves to synchronize byte order between UNIX and matlab
 load('/schroedersan/Corpora/CHiME/eval_chime_fhg/dir_struct/chime_filepaths.mat');
 nwavs = length(filepaths);
@@ -14,13 +14,17 @@ for iCounter = 1:nwavs
     [Features,header] = load_htk_with_header(fullpathread,byteswap,0);
     
     %compute deltas and double-deltas
-    Features_D  = delta_coefficients(Features',3);    %compute deltas
-    Features_DD = delta_coefficients(Features_D,2);   %compute double deltas
-    Features = [Features; Features_D'; Features_DD'];
+    %Features_D  = delta_coefficients(Features',3);    %compute deltas
+    %Features_DD = delta_coefficients(Features_D,2);   %compute double deltas
+    %Features = [Features; Features_D'; Features_DD'];
+    Features_D_DD = calc_deltas(Features,2);   %compute deltas and double deltas
+        
+    %normalize features in dimensions
+    Features_norm = fe_norm_feat(Features_D_DD);
     
     %write feature
     header.sampSize = header.sampSize*3; %because deltas and doubledeltas are added
-    write_htk_with_header(Features,header,fullpathwrite,byteswap);
+    write_htk_with_header(Features_norm,header,fullpathwrite,byteswap);
     
     % display every 1000th file to monitor the progress
     if ~mod(iCounter,1000) 
