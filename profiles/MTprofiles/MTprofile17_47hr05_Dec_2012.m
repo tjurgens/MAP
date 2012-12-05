7ca1d77bc0b10b94afa9e15d4eb77a599dae3ec5
unknown <tim@alasco.physik.uni-oldenburg.de>
2012-12-05 18:01:24 +0100

    some new profiles and UTIL_timestamp.m renamed

---
+++ profiles/MTprofiles/MTprofile17_47hr05_Dec_2012.m
@@ -0,0 +1,31 @@
+function x = MTprofile17_47hr05_Dec_2012
+%created: 17_47hr05_Dec_2012
+
+x.BFs = [6000];
+
+x.LongTone = [16.1];
+x.ShortTone = [28.4];
+
+x.Gaps = [0.01      0.03      0.05      0.07      0.09];
+x.TMCFreq = [6000];
+x.TMC = [
+27.2	 
+31.4	 
+38.9	 
+59.4	 
+61.1	 
+];
+x.TMC = x.TMC';
+
+x.MaskerRatio = [0.5      0.7      0.9        1      1.1      1.3      1.6];
+x.IFMCFreq = [6000];
+x.IFMCs = [
+75.7	 
+79.4	 
+28.2	 
+28.8	 
+34.6	 
+55.5	 
+99.7	 
+];
+x.IFMCs = x.IFMCs';
